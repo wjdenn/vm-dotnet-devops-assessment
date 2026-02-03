9ccc19c9bd69bf2019ef7c06657310191c2851fe
WillDennigan <will@Wills-MacBook-Air.local>
2026-02-03 20:24:56 +0000

    Add unit test for stats calculation

---
+++ tests/Api.Tests/StatsTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Xunit;
+
+namespace Api.Tests;
+
+public class StatsTests
+{
+    [Fact]
+    public void AverageCalculation_IsCorrect()
+    {
+        var data = new[] { 10, 20, 30, 40, 50 };
+        var average = data.Average();
+
+        Assert.Equal(30, average);
+    }
+}
